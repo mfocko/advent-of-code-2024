ef5509f4de3506b5b385c35a821d65320e8bd08b
Matej Focko <me@mfocko.xyz>
2024-12-02 14:24:52 +0100

    chore: format and lint all files
    Signed-off-by: Matej Focko <me@mfocko.xyz>

--- src/Utils.kt
+++ src/Utils.kt
@@ -5,30 +5,51 @@ import java.security.MessageDigest
 /**
  * Converts string to md5 hash.
  */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun String.md5() =
+    BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+        .toString(16)
+        .padStart(32, '0')
 
 /**
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
 
-private fun openFile(day: Int, name: String) = File("inputs/day%02d".format(day), "$name.txt")
+private fun openFile(
+    day: Int,
+    name: String,
+) = File("inputs/day%02d".format(day), "$name.txt")
 
 /**
  * Reads lines from the given input txt file.
  */
-fun readInput(day: Int, name: String) = openFile(day, name).readText().trim().lines()
+fun readInput(
+    day: Int,
+    name: String,
+) = openFile(day, name).readText().trim().lines()
 
-fun readInputAsString(day: Int, name: String) = openFile(day, name).readText()
-fun readInputAsInts(day: Int, name: String) = readInput(day, name).map { it.toInt() }
-fun readInputAsCommaSeparatedInts(day: Int, name: String) = openFile(day, name)
+fun readInputAsString(
+    day: Int,
+    name: String,
+) = openFile(day, name).readText()
+
+fun readInputAsInts(
+    day: Int,
+    name: String,
+) = readInput(day, name).map { it.toInt() }
+
+fun readInputAsCommaSeparatedInts(
+    day: Int,
+    name: String,
+) = openFile(day, name)
     .readText()
     .split(",")
     .map { it.toInt() }
 
-fun readGraph(day: Int, name: String) = readInput(day, name).fold(mapOf<String, Set<String>>()) { currentGraph, edge ->
+fun readGraph(
+    day: Int,
+    name: String,
+) = readInput(day, name).fold(mapOf<String, Set<String>>()) { currentGraph, edge ->
     val (fromVertex, toVertex) = edge.split("-")
     val fromNeighbours = currentGraph.getOrDefault(fromVertex, emptySet()) + toVertex
     val toNeighbours = currentGraph.getOrDefault(toVertex, emptySet()) + fromVertex
@@ -36,14 +57,24 @@ fun readGraph(day: Int, name: String) = readInput(day, name).fold(mapOf<String, 
     currentGraph + mapOf(fromVertex to fromNeighbours, toVertex to toNeighbours)
 }.toMap()
 
-fun <A, B> product(xs: Sequence<A>, ys: Sequence<B>): Sequence<Pair<A, B>> =
-    xs.flatMap { x -> ys.map { y -> x to y } }
+fun <A, B> product(
+    xs: Sequence<A>,
+    ys: Sequence<B>,
+): Sequence<Pair<A, B>> = xs.flatMap { x -> ys.map { y -> x to y } }
 
-fun <A, B, C> product(xs: Sequence<A>, ys: Sequence<B>, zs: Sequence<C>): Sequence<Triple<A, B, C>> =
-    xs.flatMap { x -> ys.flatMap { y -> zs.map { z -> Triple(x, y, z) } } }
+fun <A, B, C> product(
+    xs: Sequence<A>,
+    ys: Sequence<B>,
+    zs: Sequence<C>,
+): Sequence<Triple<A, B, C>> = xs.flatMap { x -> ys.flatMap { y -> zs.map { z -> Triple(x, y, z) } } }
 
-fun <A, B> product(xs: Iterable<A>, ys: Iterable<B>): Sequence<Pair<A, B>> =
-    product(xs.asSequence(), ys.asSequence())
+fun <A, B> product(
+    xs: Iterable<A>,
+    ys: Iterable<B>,
+): Sequence<Pair<A, B>> = product(xs.asSequence(), ys.asSequence())
 
-fun <A, B, C> product(xs: Iterable<A>, ys: Iterable<B>, zs: Iterable<C>): Sequence<Triple<A, B, C>> =
-    product(xs.asSequence(), ys.asSequence(), zs.asSequence())
+fun <A, B, C> product(
+    xs: Iterable<A>,
+    ys: Iterable<B>,
+    zs: Iterable<C>,
+): Sequence<Triple<A, B, C>> = product(xs.asSequence(), ys.asSequence(), zs.asSequence())
